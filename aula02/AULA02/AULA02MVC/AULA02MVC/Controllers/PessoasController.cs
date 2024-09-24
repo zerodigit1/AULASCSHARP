045152bf8590b73d162545068aa37be8b8112795
zerodigit1 <zerodigit1@yahoo.com>
2024-09-24 16:59:22 -0300

    mvc

---
+++ aula02/AULA02/AULA02MVC/AULA02MVC/Controllers/PessoasController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AULA02MVC.Controllers
+{
+    public class PessoasController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            ViewBag.TitleH1 = "Hello Pessoas";
+            return View();
+        }
+    }
+}
